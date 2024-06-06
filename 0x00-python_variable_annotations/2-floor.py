297a8883195200fc7221c6e54168c767b4761775
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-06 21:48:04 +0300

    File

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-"""
-    floor
-"""
+""" Floor """
+
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
-        returns the floor of the float
+        Returns the floor of the float
     """
     return math.floor(n)
