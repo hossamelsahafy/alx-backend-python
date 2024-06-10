0d7989adcbb601ffcc3da6516556f823b277303d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-10 19:39:42 +0300

    Files

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    asynchronous coroutine
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """async Function"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
