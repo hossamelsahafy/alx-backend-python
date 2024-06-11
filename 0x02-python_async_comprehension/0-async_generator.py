d67188e8fba81e6389f094d9fb7ed93cda666953
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-11 18:13:09 +0300

    Files

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,6 +12,6 @@ async def async_generator():
         coroutine will loop 10 times,
         each time asynchronously wait 1 second
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
